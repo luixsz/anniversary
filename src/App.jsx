b10e9a0a2b0015b9d27952faace10b8a486b9be1
luixsz <louieleecorpuz23@gmail.com>
2025-11-20 00:14:29 +0800

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useRef, useEffect } from "react";
-import songs from "../public/song.mp3";
+import songs from "./assets/song.mp3";
 import img1 from "./assets/1.jpg";
 import img2 from "./assets/2.jpg";
 import img3 from "./assets/3.jpg";
